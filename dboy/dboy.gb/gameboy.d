d89cbac694e13be11da05fd6a72f551fc0db2505
Splitty <splittydev@gmail.com>
2016-03-17 11:17:20 +0100

    Fixed tabs

--- dboy/dboy.gb/gameboy.d
+++ dboy/dboy.gb/gameboy.d
@@ -3,46 +3,46 @@ import std.stdio;
 import std.exception;
 
 struct CartHeader {
-	union EntryPoint {
-		struct CartHeaderEntry  {
-			ubyte nop;
-			ubyte jmp;
-			ushort addr;
-		}
-		CartHeaderEntry entry;
-		uint entrypoint;
-	};
-	EntryPoint entry_point;
-	ubyte nintendo_logo [48];
-	char title [11];
-	byte manufacturer_code [4];
-	ubyte cgb_flag;
-	byte license_code [2];
-	ubyte sgb_flag;
-	ubyte cartridge_type;
-	ubyte rom_size;
-	ubyte ram_size;
-	ubyte dest_code;
-	ubyte old_license_code;
-	ubyte mask_version_number;
-	ubyte header_checksum;
-	ushort global_checksum;
+    union EntryPoint {
+        struct CartHeaderEntry  {
+            ubyte nop;
+            ubyte jmp;
+            ushort addr;
+        }
+        CartHeaderEntry entry;
+        uint entrypoint;
+    };
+    EntryPoint entry_point;
+    ubyte nintendo_logo [48];
+    char title [11];
+    byte manufacturer_code [4];
+    ubyte cgb_flag;
+    byte license_code [2];
+    ubyte sgb_flag;
+    ubyte cartridge_type;
+    ubyte rom_size;
+    ubyte ram_size;
+    ubyte dest_code;
+    ubyte old_license_code;
+    ubyte mask_version_number;
+    ubyte header_checksum;
+    ushort global_checksum;
 }
 
 bool read_cart (string filename, ubyte[] buffer) {
-	File file;
-	try {
-		file = File (filename, "r");
-	} catch (ErrnoException e) {
-		writefln ("Error opening file: %s", filename);
-		return false;
-	}
-	file.byChunk (buffer);
-	return true;
+    File file;
+    try {
+        file = File (filename, "r");
+    } catch (ErrnoException e) {
+        writefln ("Error opening file: %s", filename);
+        return false;
+    }
+    file.byChunk (buffer);
+    return true;
 }
 
 CartHeader read_cart_header (ubyte[] data) {
-	auto ptr = data.ptr + 256;
-	auto headerPtr = cast (CartHeader *) ptr;
-	return *(headerPtr);
+    auto ptr = data.ptr + 256;
+    auto headerPtr = cast (CartHeader *) ptr;
+    return *(headerPtr);
 }
