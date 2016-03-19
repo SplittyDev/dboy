9efdcee345e553fb359e0a8e7999115075cfa1ec
Splitty <splittydev@gmail.com>
2016-03-19 16:45:31 +0100

    Improved memory initialization

--- dboy/gameboy/gameboy.d
+++ dboy/gameboy/gameboy.d
@@ -14,7 +14,6 @@ class Gameboy {
         cpu = new CPU ();
         mem = new Memory ();
         InitCPU ();
-        InitMemory ();
     }
 
     /// Creates a new instance of the Gameboy class.
@@ -51,40 +50,5 @@ class Gameboy {
         cpu.regs.HL.r   = 0x014D;
         cpu.regs.SP     = 0xFFFE;
         cpu.regs.PC     = 0x0100;
-    }
-
-    /// Initialize memory
-    void InitMemory () {
-        mem.Write8u (0xFF05, 0x00);
-        mem.Write8u (0xFF06, 0x00);
-        mem.Write8u (0xFF07, 0x00);
-        mem.Write8u (0xFF10, 0x80);
-        mem.Write8u (0xFF11, 0xBF);
-        mem.Write8u (0xFF12, 0xF3);
-        mem.Write8u (0xFF14, 0xBF);
-        mem.Write8u (0xFF16, 0x3F);
-        mem.Write8u (0xFF17, 0x00);
-        mem.Write8u (0xFF19, 0xBF);
-        mem.Write8u (0xFF1A, 0x7F);
-        mem.Write8u (0xFF1B, 0xFF);
-        mem.Write8u (0xFF1C, 0x9F);
-        mem.Write8u (0xFF1E, 0xBF);
-        mem.Write8u (0xFF20, 0xFF);
-        mem.Write8u (0xFF21, 0x00);
-        mem.Write8u (0xFF22, 0x00);
-        mem.Write8u (0xFF23, 0xBF);
-        mem.Write8u (0xFF24, 0x77);
-        mem.Write8u (0xFF25, 0xF3);
-        mem.Write8u (0xFF26, 0xF1);
-        mem.Write8u (0xFF40, 0x91);
-        mem.Write8u (0xFF42, 0x00);
-        mem.Write8u (0xFF43, 0x00);
-        mem.Write8u (0xFF45, 0x00);
-        mem.Write8u (0xFF47, 0xFC);
-        mem.Write8u (0xFF48, 0xFF);
-        mem.Write8u (0xFF49, 0xFF);
-        mem.Write8u (0xFF4A, 0x00);
-        mem.Write8u (0xFF4B, 0x00);
-        mem.Write8u (0xFFFF, 0x00);
     }
 }
--- dboy/gameboy/memory.d
+++ dboy/gameboy/memory.d
@@ -17,6 +17,7 @@ class Memory {
         data = cast (ubyte *) GC.calloc (size);
         GC.setAttr (data, GC.BlkAttr.NO_MOVE);
         GC.addRoot (data);
+        Init ();
     }
 
     ~this () {
@@ -71,5 +72,39 @@ class Memory {
         void *mmio = addr in write_mmio;
         if (mmio !is null)
             write_mmio [addr] (value);
+    }
+
+    void Init () {
+        Write8u (0xFF05, 0x00);
+        Write8u (0xFF06, 0x00);
+        Write8u (0xFF07, 0x00);
+        Write8u (0xFF10, 0x80);
+        Write8u (0xFF11, 0xBF);
+        Write8u (0xFF12, 0xF3);
+        Write8u (0xFF14, 0xBF);
+        Write8u (0xFF16, 0x3F);
+        Write8u (0xFF17, 0x00);
+        Write8u (0xFF19, 0xBF);
+        Write8u (0xFF1A, 0x7F);
+        Write8u (0xFF1B, 0xFF);
+        Write8u (0xFF1C, 0x9F);
+        Write8u (0xFF1E, 0xBF);
+        Write8u (0xFF20, 0xFF);
+        Write8u (0xFF21, 0x00);
+        Write8u (0xFF22, 0x00);
+        Write8u (0xFF23, 0xBF);
+        Write8u (0xFF24, 0x77);
+        Write8u (0xFF25, 0xF3);
+        Write8u (0xFF26, 0xF1);
+        Write8u (0xFF40, 0x91);
+        Write8u (0xFF42, 0x00);
+        Write8u (0xFF43, 0x00);
+        Write8u (0xFF45, 0x00);
+        Write8u (0xFF47, 0xFC);
+        Write8u (0xFF48, 0xFF);
+        Write8u (0xFF49, 0xFF);
+        Write8u (0xFF4A, 0x00);
+        Write8u (0xFF4B, 0x00);
+        Write8u (0xFFFF, 0x00);
     }
 }
